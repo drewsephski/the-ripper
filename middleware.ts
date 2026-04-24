001a1c0df4d834d86eec56e91dd171b77b00f734
drewsephski <drewsepeczi@gmail.com>
2026-04-24 09:05:01 -0500

    cleanup

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+//redirects https://exa-hallucination-detector.vercel.app to demo.exa.ai/hallucination-detector
 export function middleware(request: NextRequest) {
+  if (request.headers.get('host') === 'https://the-ripper-omega.vercel.app') {
+    return NextResponse.redirect('https://the-ripper-omega.vercel.app', {
+      status: 301
+    })
+  }
   return NextResponse.next()
 }
 export const config = {
